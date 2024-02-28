2d508fe28147d12e49e9fb11dfe37356c32315c7
ekmanss <hfers@msn.com>
2024-02-28 21:59:14 +0800

    error: call func with bytes

---
+++ script/demo.js
@@ -0,0 +1,22 @@
+const {ethers} = require("ethers");
+const HypervisorAbi = require('../abis/Hypervisor.json');
+const RoxPosnPoolAbi = require("../abis/Pool.json");
+
+const rpcUrl = "https://sepolia.blast.io"
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+const test_hypervisor = "0x10fe38d97a47887806ba54038c57fd15c734a851"
+const test_pool = "0x920261f982555a1770c76dafbdc4150771650b8b"
+const hypervisor = new ethers.Contract(test_hypervisor, HypervisorAbi, provider);
+const pool = new ethers.Contract(test_pool, RoxPosnPoolAbi, provider);
+
+async function run() {
+    let limitLower = await hypervisor.limitLower();
+    let limitUpper = await hypervisor.limitUpper();
+    let key = ethers.utils.solidityKeccak256(["address", "int24", "int24"],
+        [hypervisor.address, limitLower, limitUpper]);
+    let result = await pool.positionsSum(key)
+    console.log("result::", result)
+}
+
+run()
--- src/token-contract.ts
+++ src/token-contract.ts
@@ -7,8 +7,13 @@ import {
     Address,
     Bytes, ethereum, BigInt
 } from "@graphprotocol/graph-ts";
+import {HypervisorAbi} from "../generated/TokenContract/HypervisorAbi";
+import {PoolAbi} from "../generated/TokenContract/PoolAbi";
 
 const MyTestAddr = "0x92f533e12923d453b0e1f6020759929a3e3d02cb"
+
+const test_hypervisor = "0x10fe38d97a47887806ba54038c57fd15c734a851"
+const test_pool = "0x920261f982555a1770c76dafbdc4150771650b8b"
 
 export function handleTransfer(event: TransferEvent): void {
     let entity = new Transfer(
@@ -21,7 +26,6 @@ export function handleTransfer(event: TransferEvent): void {
     entity.blockNumber = event.block.number
     entity.blockTimestamp = event.block.timestamp
     entity.transactionHash = event.transaction.hash
-
 
 
     // let tupleArray: Array<ethereum.Value> = [
@@ -51,4 +55,22 @@ export function handleTransfer(event: TransferEvent): void {
 
     entity.blockNumber = rs_1
     entity.save()
+
+    let hypervisorContract = HypervisorAbi.bind(Address.fromString(test_hypervisor))
+    let poolContract = PoolAbi.bind(Address.fromString(test_pool))
+
+    let limitLower = hypervisorContract.limitLower()
+    let limitUpper = hypervisorContract.limitUpper()
+
+    let new_tupleArray: Array<ethereum.Value> = [
+        ethereum.Value.fromAddress(event.address),
+        ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(limitLower)),
+        ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(limitUpper)),
+    ]
+    let new_tuple = changetype<ethereum.Tuple>(new_tupleArray);
+    let new_valueFromTuple = ethereum.Value.fromTuple(new_tuple)
+    let new_encoded = ethereum.encode(new_valueFromTuple)!
+
+    let info = poolContract.positionsSum(new_encoded)
+
 }
